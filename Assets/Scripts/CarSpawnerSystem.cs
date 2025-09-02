7b6eb3be509cfbc5e822d6b6d4cea023292e4840
Vpekdas <volkan.pekdas@hotmail.com>
2025-09-02 13:49:18 +0200

    feat: Use IJobEntity in spawner too.

--- Assets/Scripts/CarSpawnerSystem.cs
+++ Assets/Scripts/CarSpawnerSystem.cs
@@ -4,63 +4,85 @@ using Unity.Transforms;
 using Unity.Mathematics;
 using Unity.Collections;
 using Unity.Rendering;
-using UnityEngine;
 
 [BurstCompile]
 public partial class CarSpawnerSystem : SystemBase
 {
+    private EntityQuery _spawnerQuery;
+    private BeginSimulationEntityCommandBufferSystem _ecbSystem;
+
     protected override void OnCreate()
     {
         RequireForUpdate<CarSpawner>();
+        _ecbSystem = World.GetOrCreateSystemManaged<BeginSimulationEntityCommandBufferSystem>();
+    }
+
+    [BurstCompile]
+    partial struct CarSpawnerJob : IJobEntity
+    {
+        public float DeltaTime;
+        public uint RandomSeed;
+        public EntityCommandBuffer.ParallelWriter ECB;
+
+        [ReadOnly] public BufferLookup<CircuitPoint> CircuitLookup;
+        [ReadOnly] public BufferLookup<ColorBufferElement> ColorLookup;
+
+        void Execute([EntityIndexInQuery] int entityInQueryIndex, Entity entity,
+                     ref CarSpawner spawner)
+        {
+            spawner.SpawnTimer += DeltaTime;
+
+            if (spawner.SpawnTimer < spawner.SpawnInterval)
+            {
+                return;
+            }
+
+            spawner.SpawnTimer = 0f;
+
+
+            Entity car = ECB.Instantiate(entityInQueryIndex, spawner.Prefab);
+
+            DynamicBuffer<ColorBufferElement> colors = ColorLookup[entity];
+            Unity.Mathematics.Random random = new(RandomSeed + (uint)entityInQueryIndex);
+            int colorIndex = random.NextInt(0, colors.Length);
+            float4 color = colors[colorIndex].Value;
+
+            if (!CircuitLookup.HasBuffer(entity))
+            {
+                return;
+            }
+
+            DynamicBuffer<CircuitPoint> circuit = CircuitLookup[entity];
+            if (circuit.Length > 0)
+            {
+                ECB.SetComponent(entityInQueryIndex, car, new LocalTransform
+                {
+                    Position = circuit[0].Position,
+                    Rotation = quaternion.identity,
+                    Scale = 1f
+                });
+            }
+            ECB.AddComponent(entityInQueryIndex, car, new MoveSpeed { Value = spawner.MoveSpeed });
+            ECB.AddComponent(entityInQueryIndex, car, new URPMaterialPropertyBaseColor { Value = color });
+            ECB.AddComponent(entityInQueryIndex, car, new CurrentTargetIndex { Value = 0 });
+            ECB.AddComponent(entityInQueryIndex, car, new RouteReference { CircuitEntity = spawner.CircuitEntity });
+        }
     }
 
     protected override void OnUpdate()
     {
         float deltaTime = SystemAPI.Time.DeltaTime;
-        EntityCommandBuffer ecb = new(Allocator.Temp);
 
-        uint seed = (uint)System.DateTime.Now.Ticks;
-        Unity.Mathematics.Random random = new(seed);
-
-        foreach (
-            (RefRW<CarSpawner> spawner, DynamicBuffer<ColorBufferElement> colors, Entity entity)
-            in SystemAPI.Query<RefRW<CarSpawner>, DynamicBuffer<ColorBufferElement>>().WithEntityAccess()
-        )
+        CarSpawnerJob job = new()
         {
-            spawner.ValueRW.SpawnTimer += deltaTime;
+            DeltaTime = deltaTime,
+            RandomSeed = (uint)System.DateTime.Now.Ticks,
+            ECB = _ecbSystem.CreateCommandBuffer().AsParallelWriter(),
+            CircuitLookup = SystemAPI.GetBufferLookup<CircuitPoint>(isReadOnly: true),
+            ColorLookup = SystemAPI.GetBufferLookup<ColorBufferElement>(),
+        };
 
-            if (spawner.ValueRW.SpawnTimer >= spawner.ValueRO.SpawnInterval)
-            {
-                spawner.ValueRW.SpawnTimer = 0f; 
-
-                Entity car = ecb.Instantiate(spawner.ValueRO.Prefab);
-                int colorIndex = random.NextInt(0, colors.Length);
-                float4 color = colors[colorIndex].Value;
-
-                DynamicBuffer<CircuitPoint> circuit = SystemAPI.GetBuffer<CircuitPoint>(entity);
-                if (circuit.Length > 0)
-                {
-                    ecb.SetComponent(car, new LocalTransform
-                    {
-                        Position = circuit[0].Position,
-                        Rotation = quaternion.identity,
-                        Scale = 1f
-                    });
-                }
-                ecb.AddComponent(car, new MoveSpeed { Value = spawner.ValueRO.MoveSpeed });
-                ecb.AddComponent(car, new URPMaterialPropertyBaseColor { Value = color });
-                ecb.AddComponent(car, new CurrentTargetIndex { Value = 0 });
-                ecb.AddComponent(car, new RouteReference { CircuitEntity = spawner.ValueRO.CircuitEntity });
-            }
-        }
-
-        ecb.Playback(EntityManager);
-        ecb.Dispose();
-    }
-
-    [BurstDiscard]
-    private void DebugLogColor(float4 color)
-    {
-        Debug.Log(color);
+        Dependency = job.ScheduleParallel(Dependency);
+        _ecbSystem.AddJobHandleForProducer(Dependency);
     }
 }
